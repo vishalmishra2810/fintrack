e14e89071df5d75cd5a073af4597b9da450bc05a
Vishal Mishra <vishalmishra@Vishals-MacBook-Air.local>
2023-09-24 21:35:57 +0530

    changes

---
+++ src/components/common/header.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import Image from "next/image";
+
+const Header = () => {
+  return (
+    <header className=" sticky top-0 z-5">
+      <div className="container mx-auto flex justify-between items-center">
+        {/* Logo with the Image component */}
+        <div className="flex items-center">
+          <h1 className="text-white text-2xl font-bold pr-2">
+            <Image src="/artist.png" alt="Logo" width={100} height={24} />
+          </h1>
+          <input
+            type="text"
+            placeholder="Search"
+            className="bg-white text-gray-800 px-2 py-1 rounded-lg"
+          />
+        </div>
+
+        {/* Navigation */}
+        <nav className="space-x-4">
+          <a href="/" className="text-white hover:underline">
+            Home
+          </a>
+          <a href="/" className="text-white hover:underline">
+            Home
+          </a>
+          <a href="/" className="text-white hover:underline">
+            Home
+          </a>
+          <a href="/" className="text-white hover:underline">
+            Home
+          </a>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/home/profile.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Image from "next/image";
+
+const Profile = () => {
+  return <div>profile</div>;
+};
+
+export default Profile;
